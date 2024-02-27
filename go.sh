3997231a61b0a0b7895877b6055a58b06148f5f0
thierry-rami <thierry.rami@laplateforme.io>
2024-02-27 10:07:39 +0100

    modif pour ~./bashrc

--- go.sh
+++ go.sh
@@ -3,8 +3,8 @@
  read -p "Saisissez le nom du compte github : " gaccount
  read -p "Saisissez le token que vous as fournis Github : " token
 
-rm ~/.ssh/id_ed25519*
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_ed25519 -C "$email" -N ""
+#rm ~/.ssh/id_ed25519*
+#ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_ed25519 -C "$email" -N ""
 
 git config --global user.email "$email"
 git config --global user.name "$gaccount"
@@ -15,5 +15,5 @@ curl -X POST -H "Authorization: token $token" \
              -d '{"title" : "SSH_Key '$dt'  Plateforme", "key" : "'"$(cat "$HOME"/.ssh/id_ed25519.pub)"'"}' \
              https://api.github.com/user/keys
 
-sed -i '/export Git_Token/d' .bashrc
+sed -i '/export Git_Token/d' ~/.bashrc
 echo 'export Git_Token="'$token'"' >> ~/.bashrc
